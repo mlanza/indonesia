c098f20c58e75a02d0e6dece94c9ded0bae5b5c2
Mario T. Lanza <mlanza@doesideas.com>
2015-06-08 15:55:49 -0400

    network spaces together

--- indo/dev/user.clj
+++ indo/dev/user.clj
@@ -1,6 +1,5 @@
 (ns user
-  "Tools for interactive development with the REPL. This file should
-  not be included in a production build of the application."
+  "Tools for interactive development with the REPL. This file should not be included in a production build of the application."
   (:require
    [clojure.java.io :as io]
    [clojure.java.javadoc :refer [javadoc]]
@@ -13,37 +12,9 @@
    [clojure.tools.namespace.repl :refer [refresh refresh-all]]
    [indo]))
 
-(def system
-  "A Var containing an object representing the application under development."
-  nil)
+(def system (atom nil))
 
-(defn init
-  "Creates and initializes the system under development in the Var #'system."
-  []
-  ;(indo.provinces)
-  )
+(defn start []
+  (reset! system (indo/init)))
 
-(defn start
-  "Starts the system running, updates the Var #'system."
-  []
-  ;; TODO
-  )
-
-(defn stop
-  "Stops the system if it is currently running, updates the Var #'system."
-  []
-  ;; TODO
-  )
-
-(defn go
-  "Initializes and starts the system running."
-  []
-  (init)
-  (start)
-  :ready)
-
-(defn reset
-  "Stops the system, reloads modified source files, and restarts it."
-  []
-  (stop)
-  (refresh :after 'user/go))
+(start)
